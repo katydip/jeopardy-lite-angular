e5e69e7b5206c0ff2be1f1e3653e1f08eee3bff0
Tate n0112138 <Terri.Tate@LibertyMutual.com>
2017-06-27 16:32:15 -0400

    get object pulled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { JeopardyService } from './jeopardy.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jeopardy Lite';
+
+  information: any[];
+
+  constructor(private JeopardyService: JeopardyService){}
+
+  getInformation(){
+    this.JeopardyService.getRecords("information")
+    //wait for Observable to be done then will do something
+    .subscribe(
+      //if {} can place more than one function call.  if no {} can only call one function
+      information => 
+      {this.information = information; 
+      console.log(this.information)
+      }
+    );
+}
+
+  ngOnInit(){
+    this.getInformation();
+  }
+
 }
