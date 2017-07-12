7b01b85476637343fd449b2ce1f91959cd48eb17
Katy DiPasquale <katy.dipasquale@libertymutual.com>
2017-07-12 16:46:06 -0400

    secure

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
